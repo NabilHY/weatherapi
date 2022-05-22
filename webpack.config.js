442020e0fd310ecc4aae4425590412e8214e3cda
nabsternab <hayounn.etd@gmail.com>
2022-05-22 23:14:44 +0100

    Deploy page

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,5 @@
-/* eslint-disable no-undef */
-/* eslint-disable no-unused-vars */
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-const productionConfig = merge([
-  {
-    output: {
-      publicPath: '/weatherapi/',
-    },
-  },
-]);
 
 module.exports = {
   entry: './src/index.js',
